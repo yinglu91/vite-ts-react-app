3433192ad67436a15bbc0f5ef7f8eefbe1119b5f
Ying Lu <yinglu5374@gmail.com>
2023-09-22 22:21:05 -0400

    no semi

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # React + TypeScript + Vite
 
+for no semi in ts:
+"editor.formatOnSave": true,
+"prettier.semi": false,
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOMClient from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const rootElement = document.getElementById('root') as HTMLElement;
-const root = ReactDOMClient.createRoot(rootElement);
+const rootElement = document.getElementById('root') as HTMLElement
+const root = ReactDOMClient.createRoot(rootElement)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
